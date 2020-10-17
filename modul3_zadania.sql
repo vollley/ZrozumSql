cb35cf96ca60b90fb26e6e84112435b312d54797
Ania Kokot <47570768+vollley@users.noreply.github.com>
2020-10-17 12:23:32 +0200

    Update modul3_zadania.sql

--- modul3_zadania.sql
+++ modul3_zadania.sql
@@ -24,17 +24,3 @@ CREATE TABLE sales (
 	added_by
 	CHECK sales_over_1k sales_amount_typ (sales_amount>1000)
 );
-
-id - typ ca³kowity, klucz g³ówny,
-? sales_date - typ data i czas (data + czêœæ godziny, minuty, sekundy), to pole ma nie zawieraæ
-wartoœci nieokreœlonych NULL,
-? sales_amount - typ zmiennoprzecinkowy (NUMERIC 38 znaków, do 2 znaków po przecinku)
-? sales_qty - typ zmiennoprzecinkowy (NUMERIC 10 znaków, do 2 znaków po przecinku)
-? product_id - typ ca³kowity INTEGER
-? added_by - typ tekstowy (nielimitowana iloœæ znaków), z wartoœci¹ domyœln¹ 'admin'
-UWAGA: nie ma tego w materia³ach wideo. Przeczytaj o atrybucie DEFAULT dla kolumny
-https://www.postgresql.org/docs/12/ddl-default.html
-? Korzystaj¹c z definiowania przy tworzeniu tabeli, po definicji kolumn, dodaje ograniczenie o
-nazwie sales_over_1k na polu sales_amount typu CHECK takie, ¿e wartoœci w polu
-sales_amount musz¹ byæ wiêksze od 1000
-
